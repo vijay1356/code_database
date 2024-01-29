c897d69a94aa51f1d5e68322c57b9b9e5f2a1f90
P L VIJAYA VITTHAL <146193592+vijay1356@users.noreply.github.com>
2024-01-29 17:41:16 +0530

    nested loop

---
+++ B_JAVA/1. notable_loops/onetofive_triangle.java
@@ -0,0 +1,19 @@
+public class onetofive_triangle {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //1st half number
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            //2nd half 
+            for(int j=2;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
