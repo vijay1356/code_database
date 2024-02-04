b3d53962b122783c013e5696f4b586bec336eb3a
P L VIJAYA VITTHAL <146193592+vijay1356@users.noreply.github.com>
2024-02-04 23:54:51 +0530

    array

--- A3_javascript/learning/e. arrays/script.js
+++ A3_javascript/learning/e. arrays/script.js
@@ -1,3 +1,15 @@
+//input the names of th 5 students
+let names= [];
+
+for(let i=0;i<5;i++){
+    names[i]=prompt("enter name");
+}
+
+console.log(names);
+
+//Qs. For a given array with marks of students -> [85, 97, 44, 37, 76, 601
+//Find the average marks of the entire class.
+
 let marks = [10,20,30,40,50,60,70,80,90,100];
 let sum=0;
 for(let i of marks){
