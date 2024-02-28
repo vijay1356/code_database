b6c52bac471f42308e2e4efb84bd46c25b2980a3
P L VIJAYA VITTHAL <146193592+vijay1356@users.noreply.github.com>
2024-02-28 23:23:36 +0530

    tricky qn

--- G_PYTHON/day1.py
+++ G_PYTHON/day1.py
@@ -7,3 +7,8 @@ print("hey \"vijay\" vitthal") #by doing this u can print even the double quotes
 # input
 a=input("enter your name :")
 print("my name is ",a)
+
+a=input("Enter the 1st number : ")
+b=input("enter the second number")
+print(a+b)
+print(a(int)+b(int))
