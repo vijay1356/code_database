e98104efdcb84198c81245101f47438c64592661
P L VIJAYA VITTHAL <146193592+vijay1356@users.noreply.github.com>
2024-02-29 08:16:01 +0530

    added the array

--- G_PYTHON/day1.py
+++ G_PYTHON/day1.py
@@ -1,15 +1,25 @@
-print("hello world") #hello world program
+print("hello world")  # hello world program
 
-# use # for comments 
+# use # for comments
 
-print("hey \"vijay\" vitthal") #by doing this u can print even the double quotes
+# by doing this u can print even the double quotes
+print("hey \"vijay\" vitthal")
 
-# input & output 
-a=input("enter your name :")
-print("my name is ",a)
+# input & output
+a = input("enter your name :")
+print("my name is ", a)
 
-# how to force print the sum of two numbers 
-a=input("Enter the 1st number : ")
-b=input("enter the second number : ")# if u use too much of your brain and type a number it will throw an error
+# how to force print the sum of two numbers
+a = input("Enter the 1st number : ")
+# if u use too much of your brain and type a number it will throw an error
+b = input("enter the second number : ")
 print(a+b)
 print(a(int)+b(int))
+
+# every string is like an array of char
+a = "harry"
+print(a[0])
+
+# u cant write an string in multiline directly u can use the """__""" for it
+print("""hello
+    bro""")
