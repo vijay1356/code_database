90b71727b5baa86c4227ece2afa41a57a10c74d0
P L VIJAYA VITTHAL <146193592+vijay1356@users.noreply.github.com>
2024-02-29 08:09:16 +0530

    added print

--- G_PYTHON/day1.py
+++ G_PYTHON/day1.py
@@ -1,14 +1,15 @@
-print("hello world")
+print("hello world") #hello world program
 
 # use # for comments 
 
 print("hey \"vijay\" vitthal") #by doing this u can print even the double quotes
 
-# input
+# input & output 
 a=input("enter your name :")
 print("my name is ",a)
 
+# how to force print the sum of two numbers 
 a=input("Enter the 1st number : ")
-b=input("enter the second number")
+b=input("enter the second number : ")# if u use too much of your brain and type a number it will throw an error
 print(a+b)
 print(a(int)+b(int))
