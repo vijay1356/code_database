a82c32c5b93747fd98ee257b1a87deb5d8a290aa
P L VIJAYA VITTHAL <146193592+vijay1356@users.noreply.github.com>
2024-03-04 12:30:55 +0530

    upper lower

--- G_PYTHON/basics runover.py
+++ G_PYTHON/basics runover.py
@@ -1,34 +1,41 @@
-print("hello world")  # hello world program
+# print("hello world")  # hello world program
 
-# use # for comments
+# # use # for comments
 
-# by doing this u can print even the double quotes
-print("hey \"vijay\" vitthal")
+# # by doing this u can print even the double quotes
+# print("hey \"vijay\" vitthal")
 
-# input & output
-a = input("enter your name :")
-print("my name is ", a)
+# # input & output
+# a = input("enter your name :")
+# print("my name is ", a)
 
-# how to force print the sum of two numbers
-a = input("Enter the 1st number : ")
-# if u use too much of your brain and type a number it will throw an error
-b = input("enter the second number : ")
-print(a+b)
-print(int(a)+int(b))
+# # how to force print the sum of two numbers
+# a = input("Enter the 1st number : ")
+# # if u use too much of your brain and type a number it will throw an error
+# b = input("enter the second number : ")
+# print(a+b)
+# print(int(a)+int(b))
 
-# every string is like an array of char
-a = "harry"
-print(a[0])
+# # every string is like an array of char
+# a = "harry"
+# print(a[0])
 
-# u cant write an string in multiline directly u can use the """__""" for it
-print("""hello
-    bro""")
+# # u cant write an string in multiline directly u can use the """__""" for it
+# print("""hello
+#     bro""")
 
-#for loop 
-for character in a:
-    print(character)
+# #for loop 
+# for character in a:
+#     print(character)
 
-# print only half or up to an index slicing 
-a="apple is a fruit"
-print(a[0:7])
-print(a[:-3]) #python interpreter considers -3 here as len(a)-3
+# # print only half or up to an index slicing 
+# a="apple is a fruit"
+# print(a[0:7])
+# print(a[:-3]) #python interpreter considers -3 here as len(a)-3
+
+#strings are immutable in python 
+a="vijay"
+print(a.upper())#upper case 
+print(a.lower())#lower case 
+print(a.rstrip("y"))
+print(a)#a remains the same and not be changed to upper case 
